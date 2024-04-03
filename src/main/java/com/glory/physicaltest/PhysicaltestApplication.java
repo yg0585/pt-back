20a0703ed8bdf497d816c297ea12553b2f8b80c1
yg0585 <yg0585@naver.com>
2024-04-03 14:59:38 +0900

    project setting

---
+++ src/main/java/com/glory/physicaltest/PhysicaltestApplication.java
@@ -0,0 +1,13 @@
+package com.glory.physicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicaltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysicaltestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glory/physicaltest/PhysicaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glory.physicaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicaltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
